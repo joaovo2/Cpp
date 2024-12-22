1251bbd5f0973c150133303b5561dda21b69eb94
Joao Vitor <joaovitoroliveirasoarespereira@gmail.com>
2024-12-22 17:56:33 -0300

    %%%

---
+++ constants/constants.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+	const double PI = 3.1415; // Declara um valor imutável chamado de 'PI'
+}
---
+++ variables/variables.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int var_value = 2; // Cria-se uma variável que guarda um número inteiro, 2.
+
+    std::string var_text = "Joao Vitor de Oliveira"; // Cria-se uma variável que guarda uma informação de texto, um nome.
+}
